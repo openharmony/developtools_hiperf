70c64a84b58378fdbf1628c412f5f206567a264e
wenlong12 <wwx1097114@DESKTOP-2021EGU.localdomain>
2022-03-18 16:36:03 +0800

    fix kora issue Signed-off-by: wenlong12 <wenlong12@huawei.com>
    Signed-off-by: wenlong12 <wwx1097114@DESKTOP-2021EGU.localdomain>

--- src/subcommand_record.cpp
+++ src/subcommand_record.cpp
@@ -945,6 +945,7 @@ bool SubCommandRecord::SendFifoAndWaitReply(const std::string &cmd)
     int fdWrite = open(CONTROL_FIFO_FILE_C2S.c_str(), O_WRONLY | O_NONBLOCK);
     if (fdWrite == -1) {
         HLOGE("can not open fifo file(%s)", CONTROL_FIFO_FILE_C2S.c_str());
+        close(fdRead);
         return false;
     }
     size_t size = write(fdWrite, cmd.c_str(), cmd.size());
@@ -952,6 +953,7 @@ bool SubCommandRecord::SendFifoAndWaitReply(const std::string &cmd)
         HLOGE("failed to write fifo file(%s) command(%s)", CONTROL_FIFO_FILE_C2S.c_str(),
               cmd.c_str());
         close(fdWrite);
+        close(fdRead);
         return false;
     }
     close(fdWrite);
