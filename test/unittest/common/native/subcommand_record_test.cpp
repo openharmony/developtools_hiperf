50ef01f7f81ba0329044c71c42e6091ee5332f08
zhangcui <zhangcui11@huawei.com>
2022-09-29 05:02:30 +0000

    fix ut test failed issue
    Signed-off-by: zhangcui <zhangcui11@huawei.com>

--- test/unittest/common/native/subcommand_record_test.cpp
+++ test/unittest/common/native/subcommand_record_test.cpp
@@ -75,7 +75,7 @@ void SubCommandRecordTest::TestRecordCommand(const std::string &option, bool exp
 
     std::string cmdString = "record ";
     if (fixPid) {
-        cmdString += "--app com.ohos.callui ";
+        cmdString += "--app com.ohos.mms ";
     }
     cmdString += " " + option;
     printf("command : %s\n", cmdString.c_str());
@@ -96,7 +96,7 @@ void SubCommandRecordTest::TestRecordCommand(const std::string &option, bool exp
 // app package name
 HWTEST_F(SubCommandRecordTest, PackageName, TestSize.Level1)
 {
-    TestRecordCommand("-d 2  --app com.ohos.callui ", true, false);
+    TestRecordCommand("-d 2  --app com.ohos.mms ", true, false);
 }
 
 HWTEST_F(SubCommandRecordTest, PackageNameErr, TestSize.Level1)
