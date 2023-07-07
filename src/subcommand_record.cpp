dc05b602628d69558e4ae153b5cc1f50693def5e
z30035271 <zhaowenqiang16@huawei.com>
2023-07-07 15:58:26 +0800

    调整参数顺序校验
    Signed-off-by: z30035271 <zhaowenqiang16@huawei.com>

--- src/subcommand_record.cpp
+++ src/subcommand_record.cpp
@@ -210,12 +210,6 @@ bool SubCommandRecord::GetOptions(std::vector<std::string> &args)
     if (!Option::GetOptionValue(args, "-c", selectCpus_)) {
         return false;
     }
-    if (!Option::GetOptionValue(args, "-p", selectPids_)) {
-        return false;
-    }
-    if (!IsExistDebugByPid(selectPids_)) {
-        return false;
-    }
     if (!Option::GetOptionValue(args, "-t", selectTids_)) {
         return false;
     }
@@ -264,6 +258,12 @@ bool SubCommandRecord::GetOptions(std::vector<std::string> &args)
         return false;
     }
     if (!Option::GetOptionValue(args, "--control", controlCmd_)) {
+        return false;
+    }
+    if (!Option::GetOptionValue(args, "-p", selectPids_)) {
+        return false;
+    }
+    if (!IsExistDebugByPid(selectPids_)) {
         return false;
     }
 
