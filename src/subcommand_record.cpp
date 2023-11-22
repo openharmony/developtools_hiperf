a1f89d371b4652fd6e69b545ce789f4634f50afb
wenlong_12 <wenlong12@huawei.com>
2023-11-22 15:58:35 +0800

    hiperf适配release app 应用 抓取trace     Signed-off-by:wenlong12<wenlong12@huawei.com>
    Signed-off-by: wenlong_12 <wenlong12@huawei.com>

--- src/subcommand_record.cpp
+++ src/subcommand_record.cpp
@@ -198,9 +198,6 @@ bool SubCommandRecord::GetOptions(std::vector<std::string> &args)
     if (!Option::GetOptionValue(args, "--app", appPackage_)) {
         return false;
     }
-    if (!IsExistDebugByApp(appPackage_)) {
-        return false;
-    }
     if (!Option::GetOptionValue(args, "--chkms", checkAppMs_)) {
         return false;
     }
@@ -211,9 +208,6 @@ bool SubCommandRecord::GetOptions(std::vector<std::string> &args)
         return false;
     }
     if (!Option::GetOptionValue(args, "-p", selectPids_)) {
-        return false;
-    }
-    if (!IsExistDebugByPid(selectPids_)) {
         return false;
     }
     if (!Option::GetOptionValue(args, "-t", selectTids_)) {
