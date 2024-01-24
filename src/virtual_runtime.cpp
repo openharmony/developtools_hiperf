16fd1bd6da3dde19f9e947fab48c1040cd171160
yuyanqinghw <yuyanqing539@huawei.com>
2024-01-24 11:26:13 +0800

    fix hiperf crash
    Signed-off-by: yuyanqinghw <yuyanqing539@huawei.com>

--- src/virtual_runtime.cpp
+++ src/virtual_runtime.cpp
@@ -55,13 +55,21 @@ std::string VirtualRuntime::ReadFromSavedCmdLines(pid_t tid)
     if (!savedCmdLines_.is_open()) {
         savedCmdLines_.open(SAVED_CMDLINES, std::ios::in);
     }
-
+    savedCmdLines_.seekg(0, std::ios::beg);
     if (savedCmdLines_.is_open()) {
         std::string line;
         std::string threadid = std::to_string(tid);
         while (getline(savedCmdLines_, line)) {
             if (line.find(threadid) != std::string::npos) {
-                return StringSplit(line, " ")[1];
+                constexpr size_t sizeLimit {2};
+                std::vector<std::string> linesToken = StringSplit(line, " ");
+                if (linesToken.size() < sizeLimit) {
+                    return EMPTY_STRING;
+                }
+                if (threadid != linesToken[0]) {
+                    continue;
+                }
+                return linesToken[1];
             }
         }
     }
