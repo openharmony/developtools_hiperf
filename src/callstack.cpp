bee5890477e69885a9c94a16f815f34a13849104
wenlong12 <wenlong12@huawei.com>
2024-03-22 09:59:41 +0800

    告警清理 Signed-off-by:wenlong12 <wenlong12@huawei.com>
    Signed-off-by: wenlong12 <wenlong12@huawei.com>

--- src/callstack.cpp
+++ src/callstack.cpp
@@ -694,9 +694,7 @@ bool CallStack::DoUnwind2(const VirtualThread &thread, std::vector<DfxFrame> &ca
         HLOGD("pc 0x%" PRIx64 " sp 0x%" PRIx64 "", frame.pc, frame.sp);
     }
     auto lastIt = callStack.end() - 1;
-    auto preIt = lastIt - 1;
-    if (lastIt != callStack.end() && preIt != callStack.end() && callStack.size() > 1 &&
-        lastIt->pc == preIt->pc && lastIt->sp == preIt->sp) {
+    if (lastIt->pc == (lastIt - 1)->pc && lastIt->sp == (lastIt - 1)->sp) {
         callStack.erase(lastIt);
         HLOGD("remove last callframe");
     }
