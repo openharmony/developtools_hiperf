7734c845d25127f71850830ab9b92394ad8c202c
wenlong_12 <wenlong12@huawei.com>
2024-04-25 12:00:42 +0000

    告警清理
    Signed-off-by: wenlong_12 <wenlong12@huawei.com>

--- include/hashlist.h
+++ include/hashlist.h
@@ -1121,7 +1121,7 @@ auto HashList<Key, Val>::AllocateNode(const Key &key, Val &&val)
     if (IsFull()) {
         pop_back();
     }
-    LinkNode<Key, Val> * pnode = LinkNode<Key, Val>::GetLinkNode(freeHead_.next_);
+    LinkNode<Key, Val> *pnode = LinkNode<Key, Val>::GetLinkNode(freeHead_.next_);
     freeHead_.next_ = freeHead_.next_->next_;
     pnode->link_.next_ = nullptr;
     pnode->link_.prev_ = nullptr;
