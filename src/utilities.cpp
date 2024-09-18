074265d3e999aa37bd10ea20c34296490e16c6ae
yuyanqinghw <yuyanqing539@huawei.com>
2024-09-18 19:41:10 +0800

    support debug app
    Signed-off-by: yuyanqinghw <yuyanqing539@huawei.com>

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -663,7 +663,11 @@ bool CheckAppIsRunning (std::vector<pid_t> &selectPids, const std::string &appPa
 
 bool IsExistDebugByApp(const std::string& bundleName)
 {
-    if (!IsSupportNonDebuggableApp() && !bundleName.empty() && !IsDebugableApp(bundleName)) {
+    std::string bundleNameTmp = bundleName;
+    if (StringEndsWith(bundleNameTmp, ":render")) {
+        bundleNameTmp = StringReplace(bundleNameTmp, ":render", "");
+    }
+    if (!IsSupportNonDebuggableApp() && !bundleNameTmp.empty() && !IsDebugableApp(bundleNameTmp)) {
         HLOGE("--app option only support debug application.");
         printf("--app option only support debug application\n");
         return false;
@@ -677,6 +681,9 @@ bool IsExistDebugByPid(const std::vector<pid_t> &pids)
     for (auto pid : pids) {
         CHECK_TRUE(pid <= 0, false, LOG_TYPE_PRINTF, "Invalid -p value '%d', the pid should be larger than 0\n", pid);
         std::string bundleName = GetProcessName(pid);
+        if (StringEndsWith(bundleName, ":render")) {
+            bundleName = StringReplace(bundleName, ":render", "");
+        }
         if (!IsSupportNonDebuggableApp() && !IsDebugableApp(bundleName)) {
             HLOGE("-p option only support debug application for %s", bundleName.c_str());
             printf("-p option only support debug application\n");
