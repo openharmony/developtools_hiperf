0f183d3a7f053aa561fcbae6a3a67f2601051671
buzhenwang <buzhenwang@huawei.com>
2025-05-09 10:44:30 +0800

    回退代码 Signed-off-by:leiguangyu <leiguangyu@huawei.com>
    Signed-off-by: buzhenwang <buzhenwang@huawei.com>

--- test/unittest/common/native/register_test.cpp
+++ test/unittest/common/native/register_test.cpp
@@ -131,6 +131,8 @@ HWTEST_F(RegisterTest, RegisterGetSPValue, TestSize.Level2)
 
     EXPECT_EQ(RegisterGetValue(value, registers, sp, sizeof(registers)),
               RegisterGetSPValue(value2, BUILD_ARCH_TYPE, registers, sizeof(registers)));
+
+    EXPECT_EQ(value, value2);
 }
 
 /**
@@ -148,6 +150,8 @@ HWTEST_F(RegisterTest, RegisterGetIPValue, TestSize.Level2)
 
     EXPECT_EQ(RegisterGetValue(value, registers, ip, sizeof(registers)),
               RegisterGetIPValue(value2, BUILD_ARCH_TYPE, registers, sizeof(registers)));
+
+    EXPECT_EQ(value, value2);
 }
 
 /**
