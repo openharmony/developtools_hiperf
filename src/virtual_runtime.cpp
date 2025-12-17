d6e00ecd601c7cad8fc9f03359e33ddc268258c0
xxuunn0032 <wangqixuan1@huawei.com>
2025-12-17 10:21:03 +0800

    DTS: DTS:DTS0000000000000 Description:hiperf split SO file
    Team:HISI_SW
    Feature or Bugfix:Feature
    
    Signed-off-by: xxuunn0032 <wangqixuan1@huawei.com>

--- src/virtual_runtime.cpp
+++ src/virtual_runtime.cpp
@@ -1032,7 +1032,7 @@ std::string VirtualRuntime::GetSoNameFromPc(uint64_t pc, std::string fileName)
         pc, [](const AdltMapDataFragment& a, uint64_t pc) {
         return a.pcEnd <= pc;
     });
-    if (soPtr == soMapPairPtr->second.end() || pc >= soPtr.pcEnd || pc < soPtr.pcBegin ) {
+    if (soPtr == soMapPairPtr->second.end() || pc >= soPtr->pcEnd || pc < soPtr->pcBegin) {
         return "";
     }
     return soPtr->originalSoName;
