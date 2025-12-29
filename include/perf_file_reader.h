c79df3a4f64e12e2bbf63fa2a94f41e5c31cb27d
xxuunn0032 <wangqixuan1@huawei.com>
2025-12-29 14:24:45 +0800

    DTS: SMO adaptation logic compatibility modification
    Signed-off-by: xxuunn0032 <wangqixuan1@huawei.com>

--- include/perf_file_reader.h
+++ include/perf_file_reader.h
@@ -71,6 +71,7 @@ private:
                           uint64_t &remainingSize, size_t &recordNumber, const perf_event_attr *attr);
     bool SMOReadRecordByAttr(ProcessRecordCB &callback, uint8_t *buf,
         uint64_t &remainingSize, size_t &recordNumber, const perf_event_attr *attr);
+    bool ValidateSMOReadRecord(uint8_t *buf, perf_event_header *header, uint64_t &remainingSize);
     bool IsValidDataFile();
     bool IsGzipFile();
 
